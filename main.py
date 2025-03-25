7b75564ab490c3252bac0d2ee2057fe7e6401439
Yellow Club 5 <hello@yellowclub.by>
2025-03-25 19:00:09 +0300

    ...

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print ("Hello")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+TOKEN="7803896890:AAFol5p5huJZ3b7bkPJCgBKOebZ3w0i0vcU"
+bot=Bot(token=TOKEN)
+dp=Dispatcher()
+async def main():
+    print ("async")
+    await dp.start_polling(bot)
+asyncio.run(main())
